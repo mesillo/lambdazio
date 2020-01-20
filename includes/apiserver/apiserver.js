fa605a4d5ae9d0cf037e54eadd909f7579cfb5dd
Alberto Mesin <alberto.mesin@fleetmatics.com>
2020-01-20 14:24:04 +0100

    Api for list create and delete.

--- includes/apiserver/apiserver.js
+++ includes/apiserver/apiserver.js
@@ -19,15 +19,92 @@ class ApiServer {
 		console.info( `ApiServer listen on ${this.port}.` );
 	}
 
+	_getGetUrl( request ) {
+		let paramsArray = request.url.split( "/" );
+		while( paramsArray[0] === "" )
+			paramsArray.shift();
+		return paramsArray;
+	}
+
+	async _listStreams( responseBody ) {
+		responseBody.action = "listStreams";
+		responseBody.status = 200;
+		responseBody.response = await this.kinesa.listStreams();
+		return responseBody;
+	}
+
+	async _createStream( responseBody ) {
+		responseBody.action = "createStream";
+		return new Promise( ( resolve, reject ) => {
+			let streamName = responseBody.parameters[1];
+			let shardNum = parseInt( responseBody.parameters[2] );
+			this.kinesa.createStream(
+				streamName,
+				shardNum
+			)
+			.then( ( result ) => {
+				responseBody.status = 200;
+				responseBody.response = result;
+				resolve( responseBody );
+			} )
+			.catch( ( error ) => {
+				responseBody.status = 500;
+				responseBody.error = error.message;
+				console.error( error );
+				resolve( responseBody );
+			} );
+		} );
+	}
+
+	async _deleteStream( responseBody ) {
+		responseBody.action = "deleteStream";
+		return new Promise( ( resolve, reject ) => {
+			let streamName = responseBody.parameters[1];
+			this.kinesa.deleteStream( streamName )
+			.then( ( result ) => {
+				responseBody.status = 200;
+				responseBody.response = result;
+				resolve( responseBody );
+			} )
+			.catch( ( error ) => {
+				responseBody.status = 500;
+				responseBody.error = error.message;
+				console.error( error );
+				resolve( responseBody );
+			} );
+		} );
+	}
+
 	async _manageGet( request, response ) {
-		console.log( request.url );
-		response.write( "GET" );
+		let responseBody = {
+			parameters: this._getGetUrl( request ),
+			action: null,
+			status: 500,
+			response: null,
+			error: null
+		};
+		switch( responseBody.parameters[0] ) {
+			case "listStreams":
+				await this._listStreams( responseBody );
+				break;
+			case "createStream":
+				await this._createStream( responseBody );
+				break;
+			case "deleteStream":
+				await this._deleteStream( responseBody );
+				break;
+			default:
+		}
+		response.statusCode = responseBody.status;
+		response.setHeader( "Content-Type", "application/json" );
+		response.setHeader( "Cache-Control", "no-store" );
+		response.write( JSON.stringify( responseBody ) );
 	}
 
 	async _requestManager( request, response ) {
 		switch( request.method ) {
 			case "GET":
-				this._manageGet( request, response );
+				await this._manageGet( request, response );
 				break;
 			default:
 				console.log( "Default" );
