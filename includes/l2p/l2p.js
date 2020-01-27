9d7f58e1e3339e183a46c35df68e17d687534b48
Alberto Mesillo Mesin <mesin.alberto@gmail.com>
2020-01-27 21:04:08 +0100

    License header

--- includes/l2p/l2p.js
+++ includes/l2p/l2p.js
@@ -1,3 +1,21 @@
+//this file contain bugs
+/**
+ * This file is part of Lambdazio.
+ * Copyright (C) yyyy  Alberto Mesillo Mesin
+ * Lambdazio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * Lambdazio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 "use strict";
 
 const fs = require( "fs" );
